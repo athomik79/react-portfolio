46abe14b7f5f27a9ec70dab28539e9ab296fcdc8
Andrew Thomas <athomik79@gmail.com>
2020-07-08 20:43:08 -0400

    Updated project section to link to deployed projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,88 +1,155 @@
 import React, { Component } from 'react';
-import projectOne from '../components/img/project-1.jpg';
-import projectTwo from '../components/img/project-2.jpg';
-import projectThree from '../components/img/project-3.jpg';
-import projectFour from '../components/img/project-4.jpg';
+import expressNote from '../components/img/express-notetaker-600.png';
+import burgerApp from '../components/img/burger-app-600.png';
+import fitnessTracker from '../components/img/fitness-tracker-600.png';
+import reactReading from '../components/img/react-reading-list-600.png';
 
 class Projects extends Component {
   render() {
     return (
       <div>
-        <section id='projects' className="projects-section p-3 p-lg-5">
-          <div className="container py-3">
-            <h2 id="title-2" className="section-title mb-5">Featured Projects</h2>
-            <div className="row">
-              <div className="col-md-3 mb-5">
-                <div className="card project-card">
-                  <div className="row no-gutters">
-                    <div className="col-lg-4 card-img-holder">
-                      <img src={projectOne} className="card-img" alt="project" />
+        <section id='projects' className='projects-section p-3 p-lg-5'>
+          <div className='container py-3'>
+            <h2 id='title-2' className='section-title mb-5'>
+              Featured Projects
+            </h2>
+            <div className='row'>
+              <div className='item col-6 col-lg-4 col-md-3 mb-5'>
+                <div className='card project-card'>
+                  <div className='row no-gutters'>
+                    <div className='col-lg-4 card-img-holder'>
+                      <img
+                        src={expressNote}
+                        className='card-img'
+                        alt='project'
+                      />
                     </div>
-                    <div className="col-lg-8">
-                      <div className="card-body">
-                        <h5 className="card-title"><a href="https://github.com/athomik79" className="theme-link">Project Heading</a></h5>
-                        <p className="card-text">Project intro lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
-                        <p className="card-text"><small className="text-muted">Client: Google</small></p>
+                    <div className='col-lg-8'>
+                      <div className='card-body'>
+                        <h5 className='card-title'>
+                          <a
+                            href='https://cryptic-reef-35449.herokuapp.com/'
+                            className='theme-link'
+                          >
+                            Express Note Taker
+                          </a>
+                        </h5>
+                        <p className='card-text'>
+                          An application built with node.js and express that can
+                          be used to write, save, and delete notes.
+                        </p>
+                        <p className='card-text'>
+                          <small className='text-muted'>GWU Bootcamp</small>
+                        </p>
                       </div>
                     </div>
                   </div>
-                </div>{/*//card*/}
-              </div>{/*//col*/}
-              <div className="col-md-3 mb-5">	
-                <div className="card project-card">
-                  <div className="row no-gutters">
-                    <div className="col-lg-4 card-img-holder">
-                      <img src={projectTwo} className="card-img" alt="project" />
+                </div>
+                {/*//card*/}
+              </div>
+              {/*//col*/}
+              <div className='item col-6 col-lg-4 col-md-3 mb-5'>
+                <div className='card project-card'>
+                  <div className='row no-gutters'>
+                    <div className='col-lg-4 card-img-holder'>
+                      <img src={burgerApp} className='card-img' alt='project' />
                     </div>
-                    <div className="col-lg-8">
-                      <div className="card-body">
-                        <h5 className="card-title"><a href="https://github.com/athomik79" className="theme-link">Project Heading</a></h5>
-                        <p className="card-text">Project intro lorem ipsum dolor sit amet, consectetuer adipiscing elit. </p>
-                        <p className="card-text"><small className="text-muted">Client: Dropbox</small></p>
+                    <div className='col-lg-8'>
+                      <div className='card-body'>
+                        <h5 className='card-title'>
+                          <a
+                            href='https://hidden-spire-81956.herokuapp.com/'
+                            className='theme-link'
+                          >
+                            Eat Da Burger
+                          </a>
+                        </h5>
+                        <p className='card-text'>
+                          Eat Da Burger is a CRUD app that lets users create,
+                          read, update and delete the names of burgers.{' '}
+                        </p>
+                        <p className='card-text'>
+                          <small className='text-muted'>GWU Bootcamp</small>
+                        </p>
                       </div>
                     </div>
                   </div>
-                </div>{/*//card*/}
-              </div>{/*//col*/}
-              <div className="col-md-3 mb-5">
-                <div className="card project-card">
-                  <div className="row no-gutters">
-                    <div className="col-lg-4 card-img-holder">
-                      <img src={projectThree} className="card-img" alt="project" />
+                </div>
+                {/*//card*/}
+              </div>
+              {/*//col*/}
+              <div className='item col-6 col-lg-4 col-md-3 mb-5'>
+                <div className='card project-card'>
+                  <div className='row no-gutters'>
+                    <div className='col-lg-4 card-img-holder'>
+                      <img
+                        src={fitnessTracker}
+                        className='card-img'
+                        alt='project'
+                      />
                     </div>
-                    <div className="col-lg-8">
-                      <div className="card-body">
-                        <h5 className="card-title"><a href="https://github.com/athomik79" className="theme-link">Project Heading</a></h5>
-                        <p className="card-text">Project intro lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
-                        <p className="card-text"><small className="text-muted">Client: Google</small></p>
+                    <div className='col-lg-8'>
+                      <div className='card-body'>
+                        <h5 className='card-title'>
+                          <a href='https://intense-shore-21076.herokuapp.com/?id=5f06617b3420b2001792a89a'>
+                            Nosql Workout Tracker
+                          </a>
+                        </h5>
+                        <p className='card-text'>
+                          A workout tracker application built with Nodejs,
+                          Express, and MongoDB.
+                        </p>
+                        <p className='card-text'>
+                          <small className='text-muted'>GWU Bootcamp</small>
+                        </p>
                       </div>
                     </div>
                   </div>
-                </div>{/*//card*/}
-              </div>{/*//col*/}
-              <div className="col-md-3 mb-5">
-                <div className="card project-card">
-                  <div className="row no-gutters">
-                    <div className="col-lg-4 card-img-holder">
-                      <img src={projectFour} className="card-img" alt="project" />
+                </div>
+                {/*//card*/}
+              </div>
+              {/*//col*/}
+              <div className='item col-6 col-lg-4 col-md-3 mb-5'>
+                <div className='card project-card'>
+                  <div className='row no-gutters'>
+                    <div className='col-lg-4 card-img-holder'>
+                      <img
+                        src={reactReading}
+                        className='card-img'
+                        alt='project'
+                      />
                     </div>
-                    <div className="col-lg-8">
-                      <div className="card-body">
-                        <h5 className="card-title"><a href="https://github.com/athomik79" className="theme-link">Project Heading</a></h5>
-                        <p className="card-text">Project intro lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
-                        <p className="card-text"><small className="text-muted">Client: Uber</small></p>
+                    <div className='col-lg-8'>
+                      <div className='card-body'>
+                        <h5 className='card-title'>
+                          <a href='https://powerful-tundra-64148.herokuapp.com/'>
+                            MERN Reading List APP
+                          </a>
+                        </h5>
+                        <p className='card-text'>
+                          A full stack reading list application, made with
+                          MongoDB, Express, React and Nodejs.
+                        </p>
+                        <p className='card-text'>
+                          <small className='text-muted'>GWU Bootcamp</small>
+                        </p>
                       </div>
                     </div>
                   </div>
-                </div>{/*//card*/}
-              </div>{/*//col*/}
-            </div>{/*//row*/}
+                </div>
+                {/*//card*/}
+              </div>
+              {/*//col*/}
+            </div>
+            {/*//row*/}
             {/* <div className="text-center py-3"><a href="portfolio.html" className="btn btn-primary"><i className="fas fa-arrow-alt-circle-right mr-2" />View Portfolio</a></div>	 */}
-          </div>{/*//container*/}
-        </section>{/*//featured-section*/}
+          </div>
+          {/*//container*/}
+        </section>
+        {/*//featured-section*/}
       </div>
     );
   }
-};
+}
 
 export default Projects;
