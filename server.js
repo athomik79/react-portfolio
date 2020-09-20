cb59d0c3b55e0c83f4572a2457b0a392629caa8a
Andrew Thomas <athomik79@gmail.com>
2020-09-20 16:00:04 -0400

    Updated npm packages

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
+const sslRedirect = require('heroku-ssl-redirect');
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
-const { google } = require('googleapis');
-const OAuth2 = google.auth.OAuth2;
+// const { google } = require('googleapis');
+// const OAuth2 = google.auth.OAuth2;
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(sslRedirect());
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
