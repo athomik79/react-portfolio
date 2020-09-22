53173a6aa0991871cb89b1c7051e87bcd56e160b
Andrew Thomas <athomik79@gmail.com>
2020-09-22 16:23:24 -0400

    Updated server.js

--- server.js
+++ server.js
@@ -10,24 +10,25 @@ const nodemailer = require('nodemailer');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-// add middleware
-app.use(express.static('public'));
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-}
-
 // app.use(sslRedirect());
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // app.get('/', function (req, res) {
 //   res.send({
 //     message: 'Default route for email',
 //   });
-// });
-
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 // });
 
 http.createServer(app).listen(PORT, function (req, res) {
