71a9e38a008c61fdb23622b39a410e50d5d284dd
Andrew Thomas <athomik79@gmail.com>
2020-09-22 16:47:04 -0400

    Production Build

---
+++ client/build/precache-manifest.1a7c037aeebd075b9593be2ff72629a3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bed94a3c9b94a97b6001fb199a4949f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29e637a511cfd71504a8",
+    "url": "/static/css/2.43079d8c.chunk.css"
+  },
+  {
+    "revision": "e0ee3edc2c38800002e6",
+    "url": "/static/css/main.dff3c982.chunk.css"
+  },
+  {
+    "revision": "29e637a511cfd71504a8",
+    "url": "/static/js/2.a79f829a.chunk.js"
+  },
+  {
+    "revision": "b396b9c7cb89986c0edcd31803c9a394",
+    "url": "/static/js/2.a79f829a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0ee3edc2c38800002e6",
+    "url": "/static/js/main.c1003ae2.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "0b54603fa6dc1004caff50e57ec830aa",
+    "url": "/static/media/green_mountain_logo_512.0b54603f.png"
+  },
+  {
+    "revision": "bbfee0b64eb93ad4b363fd184cc1d556",
+    "url": "/static/media/logo.bbfee0b6.png"
+  }
+]);
